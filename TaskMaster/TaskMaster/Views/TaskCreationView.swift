35dcb4cfdc104585c5d9f7eb6a79bbd892a2d14d
Felippo Stedile <felippostedile@10-41-49-83.0200.epoa.br.iosda.org>
2023-09-12 18:11:52 -0300

    grande progresso

--- TaskMaster/TaskMaster/Views/TaskCreationView.swift
+++ TaskMaster/TaskMaster/Views/TaskCreationView.swift
@@ -12,17 +12,40 @@ enum Period: String, CaseIterable, Identifiable {
     var id: Self { self }
 }
 
+enum Week: Int {
+    case sunday = 0
+    case monday = 1
+    case tuesday = 2
+    case wednesday = 3
+    case thursday = 4
+    case friday = 5
+    case saturday = 6
+    
+    var dayLetter: String {
+        switch self{
+        case .sunday: return "S"
+        case .monday: return "M"
+        case .tuesday: return "T"
+        case .wednesday: return "W"
+        case .thursday: return "T"
+        case .friday: return "F"
+        case .saturday: return "S"
+        }
+    }
+    
+}
+
 struct TaskCreationView: View {
     var taskName: String
     var taskDueDate: Date
     //var weekDays: [Week]
-    var monthDays: [Int]
+    @State var monthDays: [Int]
     @State private var selectedPeriod: Period = .weekly
-    @State var monday: Bool = false
-    @State var tuesday: Bool = false
-    @State var wednesday: Bool = false
-    @State var thursday: Bool = false
-    @State var friday: Bool = false
+    @State var selectedWeek: [Week]
+    @State private var date = Date()
+    @State private var showCalendar: Bool = false
+    
+    let weekDays: [Week] = [.sunday, .monday, .tuesday, .wednesday, .thursday, .friday, .saturday]
 
 
 
@@ -40,7 +63,7 @@ struct TaskCreationView: View {
                     .fontWeight(.semibold)
             }
             
-            VStack {
+            VStack(alignment: .leading) {
                 Picker("Period", selection: $selectedPeriod) {
                     Text("Weekly").tag(Period.weekly)
                     Text("Monthly").tag(Period.monthly)
@@ -48,54 +71,40 @@ struct TaskCreationView: View {
             
                 if selectedPeriod == .weekly {
                     HStack {
-                        ForEach(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"], id: \.self){ day in
+                        ForEach(weekDays, id: \.self){ day in
                             Button {
-                                monday.toggle()
+                                selectedWeek.contains(day) ? selectedWeek = selectedWeek.filter {$0 != day} : selectedWeek.append(day)
                             } label: {
                                 ZStack{
                                     Circle().stroke()
-                                        .frame(height: 20)
-                                    Text(day.dropLast(2))
+                                        .frame(height: 25)
+                                    Text(day.dayLetter)
                                 }
-                                .foregroundColor(monday ? .blue : .primary)
+                                .foregroundColor(selectedWeek.contains(day) ? .blue : .primary)
+                            }.buttonStyle(.plain)
+                        }
+                    }
+                } else {
+                    HStack {
+                        ForEach(monthDays, id: \.self){ day in
+                            Button {
+                                monthDays = monthDays.filter{$0 != day}
+                            } label: {
+                                ZStack{
+                                    Circle().stroke()
+                                        .frame(height: 25)
+                                    Text("\(day)")
+                                }
                             }.buttonStyle(.plain)
                         }
                         Button {
-                            tuesday.toggle()
+                            showCalendar = true
                         } label: {
-                            ZStack{
-                                Circle().stroke()
-                                    .frame(height: 20)
-                                Text("M")
-                            }
-                            .foregroundColor(tuesday ? .blue : .primary)
-                        }.buttonStyle(.plain)
-                        ZStack{
-                            Circle().stroke()
-                                .frame(height: 20)
-                            Text("T")
+                            Image(systemName: "plus")
                         }
-                        ZStack{
-                            Circle().stroke()
-                                .frame(height: 20)
-                            Text("W")
-                        }
-                        ZStack{
-                            Circle().stroke()
-                                .frame(height: 20)
-                            Text("T")
-                        }
-                        ZStack{
-                            Circle().stroke()
-                                .frame(height: 20)
-                            Text("F")
-                        }
-                        ZStack{
-                            Circle().stroke()
-                                .frame(height: 20)
-                            Text("S")
-                        }
+
                     }
+                    
                 }
                 HStack{
                     ProgressView(value:0.1)
@@ -103,11 +112,51 @@ struct TaskCreationView: View {
                 }
             }
         }
+        .sheet(isPresented: $showCalendar) {
+            monthDays.sort()
+        } content: {
+                VStack{
+                    ForEach(0..<4) { line in
+                        HStack{
+                            ForEach(1..<8){item in
+                                let day = item + line * 7
+                                Button{
+                                    monthDays.contains(day) ? monthDays = monthDays.filter {$0 != day} : monthDays.count < 10 ?  monthDays.append(day) : nil
+                                } label:
+                                {
+                                    ZStack{
+                                        Circle().stroke()
+                                            .frame(height: 40)
+                                        Text("\(day)")
+                                    }
+                                    .foregroundColor(monthDays.contains(day) ? .blue : .primary)
+                                }.buttonStyle(.plain)
+                        }
+                    }
+                }
+                    #warning("Fazer a view de edit ter a sheet por padrao, e a view do elemento normal ser só as bolinhas")
+                    HStack{
+                        ForEach(29..<32) {rest in
+                            Button{
+                                monthDays.contains(rest) ? monthDays = monthDays.filter {$0 != rest} : monthDays.count < 10 ? monthDays.append(rest) : nil
+                            } label:
+                            {
+                                ZStack{
+                                    Circle().stroke()
+                                        .frame(height: 40)
+                                    Text("\(rest)")
+                                }
+                                .foregroundColor(monthDays.contains(rest) ? .blue : .primary)
+                            }.buttonStyle(.plain)
+                        }
+                    }
+            }
+        }
     }
 }
 
 struct TaskCreationView_Previews: PreviewProvider {
     static var previews: some View {
-        TaskCreationView(taskName: "Oi", taskDueDate: Date(), monthDays: [10, 20])
+        TaskCreationView(taskName: "Oi", taskDueDate: Date(), monthDays: [10, 20], selectedWeek: [])
     }
 }
